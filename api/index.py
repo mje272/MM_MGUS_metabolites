733ce94f74d368047376f41ac599c1d2be8ba37c
Michael Egan <151584575+mje272@users.noreply.github.com>
2025-06-06 08:31:45 -0700

    add results page

--- api/index.py
+++ api/index.py
@@ -8,5 +8,11 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/query_metabolite', methods = ['POST'])
+def query_metabolite():
+    query = request.form['query_metabolite']
+    return render_template('query_result.html', metabolite=query_metabolite)
+
+
 if __name__ == "__main__":
     app.run(debug = True)
