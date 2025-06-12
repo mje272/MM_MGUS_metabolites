4ea2575c7391c3e1004d51de020fd30733dd1757
Michael Egan <151584575+mje272@users.noreply.github.com>
2025-06-12 07:07:56 -0700

    query results

--- api/index.py
+++ api/index.py
@@ -11,7 +11,11 @@ def index():
 @app.route('/query_metabolite', methods = ['POST'])
 def query_metabolite():
     metabolite = request.form['query_metabolite']
-    return render_template('query_result.html', metabolite=metabolite)
+    ###### get this from dynamodb via lambda function
+    mm_data = []
+    mgus_data = []
+    ######
+    return render_template('query_result.html', metabolite=metabolite, mm_data=mm_data, mgus_data=mgus_data)
 
 
 if __name__ == "__main__":
